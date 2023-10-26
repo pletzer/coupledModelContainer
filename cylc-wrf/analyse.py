d86b0cba67cfcfd2413430d1cc60a25b3ba6109d
Alex Pletzer <alexander@gokliya.net>
2023-10-26 07:56:48 +0000

    comparing apptainer vs native perf

--- cylc-wrf/analyse.py
+++ cylc-wrf/analyse.py
@@ -20,8 +20,8 @@ exit_times = []
 exec_times = []
 job_ids = []
 ntasks = []
+execs = []
 success = []
-nthreads = []
 for sf in status_files:
 
   print(f'... status file: {sf}')
@@ -30,13 +30,13 @@ for sf in status_files:
   exit_time = float('inf')
   job_id = -1
   nw = 0
-  nt = 0
+  exe = '?'
   scs = '?'
-  m = re.search(r'run(\d+)p_(\d+)t\_', sf)
+  m = re.search(r'run\_(\w+)(\d+)n\_', sf)
   if m:
-    nt = int(m.group(2))
-    nw = int(m.group(1))
-    print(f'......got ntasks = {nw} and nthreads = {nt}')
+    exe = m.group(1)
+    nodes = int(m.group(2))
+    print(f'......got nodes = {nodes}')
 
   for line in open(sf).readlines():
 
@@ -58,8 +58,8 @@ for sf in status_files:
   init_times.append(init_time)
   exit_times.append(exit_time)
   job_ids.append(job_id)
-  ntasks.append(nw)
-  nthreads.append(nt)
+  ntasks.append(nodes*20)
+  execs.append(exe)
   success.append(scs)
   try:
     exec_times.append( (exit_time - init_time).seconds )
@@ -70,14 +70,16 @@ df = pd.DataFrame({'job_id': job_ids,
                    'exec time (s)': exec_times, 
 	           'ntasks': ntasks,
 	           'success': success,
-	           'nthreads': nthreads})
+		   'exec_type': execs})
 df.to_csv(f'{case}.csv')
 
 # print all the runs that failed
 print(df[df.success == 'EXIT'])
 
 dfs = df[(df.success == 'SUCCEEDED')]
-sn.lineplot(data=dfs, x='ntasks', y='exec time (s)', errorbar='sd', hue='nthreads')
+# print all the runs that succeeded
+print(dfs)
+sn.lineplot(data=dfs, x='ntasks', y='exec time (s)', errorbar='sd', hue='exec_type')
 plt.xlim([1, dfs.ntasks.max()])
 plt.ylim([0, dfs['exec time (s)'].max()])
 plt.title(f'WRF apptainer (cyclone Gabrielle 2 days)')
