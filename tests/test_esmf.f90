edff6283478d16bc490c511b77fe8bea05740d27
Alex Pletzer <alexander@gokliya.net>
2023-11-23 00:20:37 +0000

    extending the test

--- tests/test_esmf.f90
+++ tests/test_esmf.f90
@@ -2,10 +2,16 @@ program test
 	use ESMF
 	use NUOPC
 	implicit none
-	integer :: rc
+	integer :: rc, mypet,  npets
 	type(ESMF_VM) :: vm
 	call ESMF_Initialize(logkindflag=ESMF_LOGKIND_MULTI,              &
                            defaultCalkind=ESMF_CALKIND_GREGORIAN,       &
                            vm=vm, rc=rc)
+
+        call ESMF_VMGet(vm=vm, localPet=mypet, petCount=npets, rc=rc)
+        if (rc /= 0) print *,'ERROR: rc = ', rc
+
+        print *, '[', mypet, '] out of ', npets
+
 	call ESMF_Finalize(rc=rc)
 end program test
